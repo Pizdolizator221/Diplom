1f9730b2e2c5dd0860bf01b581ceaeee35ec5012
zhopa123 <nikgawrik@yandex.ru>
2021-06-07 12:43:29 +0800

    threads list

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,32 @@
     <div class="container mt-5">
       <h1 class="display-4 text-center">Main page</h1>
       <p class="text-center lead">
-        Наша компания занимается страховкой уже 25 лет <br>
-        Наш адресс: г.Ангарск, 52 кв-л, д.1 <br>
-        Телефон: 88005553535 <br>
+        aboba <br>
+        aboba <br>
+        amogus <br>
       </p>
     </div>
+
+    <section>
+      <div class="jumbotron" v-for="thread in threads" v-bind:key="thread._id">
+        {{ thread.contentText }}
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+  import axios from 'axios';
+
+  export default {
+    name: 'Home',
+    data () {
+      return {
+        threads: null
+      };
+    },
+    mounted () {
+      axios.get('http://localhost:4000/api/threads').then(res => (this.threads = res.data));
+    }
+  }
 </script>

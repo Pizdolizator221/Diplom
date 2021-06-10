f429408df6e95dec14aad1897888639d61971dd8
Pizdolizator221 <chungus@DESKTOP-K72F2S0.localdomain>
2021-06-10 16:53:48 +0800

    font awesome!

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
 import BootstrapVue from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faArrowLeft, faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import store from './store/index';
 import Axios from 'axios';
@@ -13,6 +18,9 @@ const token = localStorage.getItem('token');
 if (token) {
   Vue.prototype.$http.defaults.headers.common['Authorization'] = token;
 }
+
+library.add(faArrowLeft, faStar);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
--- src/views/Threads/Thread.vue
+++ src/views/Threads/Thread.vue
@@ -1,7 +1,12 @@
 <template>
     <fieldset class="jumbotron mt-5">
         <legend class="text-center display-4">
+            <router-link to="/feed">
+                <font-awesome-icon icon="arrow-left" />
+            </router-link>
             {{thread.title}}
+            
+            <font-awesome-icon v-if="isYourThread" icon="star" />
         </legend>
 
         <div class="container">
@@ -29,6 +34,13 @@ export default ({
     mounted() {
         axios.get('http://localhost:4000/api/threads/find_by_id', {params: {_id: this._id}})
             .then(res => this.thread = res.data);
+    },
+    computed: {
+        isYourThread() {
+            const user = localStorage.getItem('user');
+            const username = JSON.parse(user).username;
+            return username == this.thread.authorUsername ? true : false
+        }
     }
 })
 </script>
